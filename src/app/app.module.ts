1e452aa802eb496d5ffd9fae07b9a5a1a914167b
Edson Machado <edson.machado@soprasteria.com>
2019-07-25 11:47:02 +0200

    feat adding main view & first components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MainViewComponent } from './views/main-view/main-view.component';
+import { HeaderBarComponent } from './components/header-bar/header-bar.component';
+import { ItemsSearchComponent } from './components/items-search/items-search.component';
+import { InputSearchComponent } from './components/input-search/input-search.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainViewComponent,
+    HeaderBarComponent,
+    ItemsSearchComponent,
+    InputSearchComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/items-search/items-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mf-items-search',
+  templateUrl: './items-search.component.html',
+  styleUrls: ['./items-search.component.scss']
+})
+export class ItemsSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
