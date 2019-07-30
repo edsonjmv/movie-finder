c7b5e53061741c41e5b86a9a4d139828a5a15abd
Edson Machado <edson.machado@soprasteria.com>
2019-07-30 08:19:27 +0200

    refactor change order for empty message

--- src/app/components/items-finder/items-finder.component.ts
+++ src/app/components/items-finder/items-finder.component.ts
@@ -10,14 +10,14 @@ import { Item } from '../../models/item';
       (submitSearch)="submitSearch.emit($event)">
     </input-search>
 
-    <items-list
-      [items]="items">
-    </items-list>
-
     <p class="empty-message" *ngIf="!items || items.length === 0">
       <ng-container *ngIf="loading; else noResults">Loading...</ng-container>
       <ng-template #noResults>No results found.</ng-template>
     </p>
+
+    <items-list
+      [items]="items">
+    </items-list>
   `,
   styleUrls: ['./items-finder.component.scss']
 })
