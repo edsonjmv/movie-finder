7754770a66239a7e0ba58f398bcd1af3736a9e86
Edson Machado <edson.machado@soprasteria.com>
2019-08-02 13:57:02 +0200

    refactor initialize item class in constructor

--- src/app/models/item.ts
+++ src/app/models/item.ts
@@ -3,5 +3,9 @@ export class Item {
     public image: string,
     public subtitle: string,
     public title: string
-  ) { };
+  ) {
+    this.image = image;
+    this.subtitle = subtitle;
+    this.title = title;
+  }
 }
